0a3cd6fc62e9bddf8dd3439dfbe6e7601e706e9d
Samuel Deegan <samueldeegan@Samuels-MacBook-Pro.local>
2025-07-29 11:01:25 -0400

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -5,7 +5,31 @@ calculator.py
 One function per operation, in order.
 """
 # First example
+import math
 def add(a, b): 
-    pass
+    return a+b
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError('Cannot divide by 0')
+    else:
+        return b / a
+
+def log(a, b):
+    if a <= 0 or a == 1:
+        raise ValueError("Logarithm base must be greater than 0 and not equal to 1.")
+    if b <= 0:
+        raise ValueError("Logarithm argument must be greater than 0.")
+
+    return math.log(b,a) # use math library + raise ValueError
+
+def exp(a, b):
+    return a**b
+
 
 
