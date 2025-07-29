57612e1f8319591d2c9833d4cd6115a9e1bfe0f4
Huaixi Sun <lieqtaot@Huaixis-MacBook-Air.local>
2025-07-29 18:30:47 -0400

    square root and hypotenuse added

--- calculator.py
+++ calculator.py
@@ -6,6 +6,15 @@ One function per operation, in order.
 """
 # First example
 import math
+def square_root(a):
+    if a < 0:
+        raise ValueError("No square root")
+    else:
+        return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
 def add(a, b): 
     return a+b
 def sub(a, b):
