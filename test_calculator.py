6a65e7859cbc7da8d67e8442265d54aea3754451
Huaixi Sun <lieqtaot@Huaixis-MacBook-Air.local>
2025-07-29 19:20:09 -0400

    test calculator p1

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import *
+import calculator
 
 class TestCalculator(unittest.TestCase):
     ######### Partner 2
@@ -10,13 +10,14 @@ class TestCalculator(unittest.TestCase):
     #     fill in code
     # ##########################
 
-    ######## Partner 1
-    # def test_multiply(self): # 3 assertions
-    #     fill in code
-
-    # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
+    def test_multiply(self):
+        self.assertEqual(calculator.mul(2, 3), 6)
+        self.assertEqual(calculator.mul(0, 1), 0)
+        self.assertEqual(calculator.mul(-1, 1), -1)
+    def test_divide(self):
+        self.assertEqual(calculator.div(2, 3), 1)
+        self.assertEqual(calculator.div(0, 1), 0)
+        self.assertEqual(calculator.div(-1, 1), -1)
 
     ######## Partner 2
     # def test_divide_by_zero(self): # 1 assertion
@@ -34,22 +35,20 @@ class TestCalculator(unittest.TestCase):
     # ##########################
     
     ######## Partner 1
-    # def test_log_invalid_argument(self): # 1 assertion
-    #     # call log function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     logarithm(0, 5)
-    #     fill in code
+    def test_log_invalid_argument(self):
+        with self.assertRaises(ValueError):
+            calculator.log(1, 0)
 
-    # def test_hypotenuse(self): # 3 assertions
-    #     fill in code
+    def test_hypotenuse(self):
+        self.assertAlmostEqual(calculator.hypotenuse(3, 4), 5)
+        self.assertAlmostEqual(calculator.hypotenuse(-3, -4), 5)
+        self.assertAlmostEqual(calculator.hypotenuse(5, 12), 13)
 
-    # def test_sqrt(self): # 3 assertions
-    #     # Test for invalid argument, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #    square_root(NUM)
-    #     # Test basic function
-    #     fill in code
-    ##########################
+    def test_sqrt(self):
+        self.assertEqual(calculator.square_root(1), 1)
+        self.assertEqual(calculator.square_root(0), 0)
+        with self.assertRaises(ValueError):
+            calculator.square_root(-1)
 
 # Do not touch this
 if __name__ == "__main__":
